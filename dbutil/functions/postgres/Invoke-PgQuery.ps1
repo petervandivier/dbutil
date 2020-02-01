d005a96eb882b00ea692e4d0dd6e30f11da9972d
petervandivier <petervandivier@gmail.com>
2020-02-01 12:29:58 +0000

    add port, user, pw

--- dbutil/functions/postgres/Invoke-PgQuery.ps1
+++ dbutil/functions/postgres/Invoke-PgQuery.ps1
@@ -2,6 +2,16 @@ function Invoke-PgQuery {
 <#
 .DESCRIPTION
     Sends a text string to a postgres database and tries to return a dataset
+
+.EXAMPLE
+    # return version() from a docker instance port forwarded to 54320:5432
+    $splat = @{
+        Port = 54320 
+        User = 'postgres' 
+        Password = (ConvertTo-SecureString -AsPlainText 'example' -Force)
+    }
+    Invoke-PgQuery @splat
+
 #>
     [CmdletBinding()]
     param (
@@ -14,8 +24,18 @@ function Invoke-PgQuery {
         [string]
         $SQLConnection = '127.0.0.1',
 
+        [int]
+        [ValidateRange(1024,65535)]
+        $Port = 5432,
+
         [string]
-        $Database = 'postgres'
+        $Database = 'postgres',
+
+        [string]
+        $User = $env:USER,
+
+        [SecureString]
+        $Password
     )
 
     $iniFile = Invoke-Expression "odbcinst -j | grep DRIVERS | awk '{print `$2}'"
@@ -25,10 +45,24 @@ function Invoke-PgQuery {
         return
     }
 
+    # Pwd is stripped from the connection string at .conn() but is still sent in the clear
+    # over the network unless via SSL. 
+    $BSTR = [system.runtime.interopservices.marshal]::SecureStringToBSTR($Password)
+
     $conn = New-Object System.Data.Odbc.OdbcConnection
-    $conn.ConnectionString="Driver={$Driver};Server=$SQLConnection;Port=5432;Database=$Database;"
+    $conn.ConnectionString = @(
+        "Driver={$Driver};"
+        "Server=$SQLConnection;"
+        "Port=$Port;"
+        "Database=$Database;"
+        "Uid=$User;"
+        "Pwd=$([system.runtime.interopservices.marshal]::PtrToStringAuto($BSTR));"
+    ) -join ''
     $conn.Open()
-    
+
+    [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR) 
+    Remove-Variable Password,BSTR
+
     $cmd = New-Object System.Data.Odbc.OdbcCommand($Query,$conn)
     $ds = New-Object System.Data.DataSet
     $da = New-Object System.Data.Odbc.OdbcDataAdapter($cmd)
